ebfc91c60456a87c8c64a580f6bb5fede8c17434
AdrianValladares <a.valladares.d@gmail.com>
2022-03-07 15:56:23 +0100

    added error handling

--- src/server/launchers/launchExpressApp.js
+++ src/server/launchers/launchExpressApp.js
@@ -1,9 +1,14 @@
 const debug = require("debug")("SCHEDBORD:express-launcher");
 const express = require("express");
+const generalError = require("../middleware/generalError");
+const notFound = require("../middleware/notFound");
 
 const launchExpressApp = () => {
   debug("launching express app...");
   const app = express();
+
+  app.use(notFound);
+  app.use(generalError);
 
   return app;
 };
