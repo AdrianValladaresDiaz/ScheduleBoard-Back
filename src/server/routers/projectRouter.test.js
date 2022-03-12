50873851601e6e202f635728c39e7dfb856bf3a0
AdrianValladares <a.valladares.d@gmail.com>
2022-03-12 13:14:53 +0100

    added test for projectRouter

---
+++ src/server/routers/projectRouter.test.js
@@ -0,0 +1,43 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { default: mongoose } = require("mongoose");
+const request = require("supertest");
+const connectToDataBase = require("../../database");
+const { Project } = require("../../database/models/Project");
+const { fakeProject } = require("../utils/testingUtils");
+const launchExpressApp = require("../launchers/launchExpressApp");
+
+let mongoServer;
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const connectionString = mongoServer.getUri();
+  await connectToDataBase(connectionString);
+});
+
+beforeEach(async () => {
+  await Project.create(fakeProject);
+});
+
+afterEach(async () => {
+  await Project.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+describe("Given projectRouter endpoint", () => {
+  describe("When it receives a request at '/project' with a correct projectId", () => {
+    test("Then it should return that project", async () => {
+      const app = launchExpressApp();
+      // eslint-disable-next-line no-underscore-dangle
+      const correctProjectId = fakeProject._id;
+      const expectedProject = { ...fakeProject };
+      const { body } = await request(app)
+        .get("/project")
+        .send({ projectId: correctProjectId });
+
+      expect(expectedProject).toEqual(body.message);
+    });
+  });
+});
